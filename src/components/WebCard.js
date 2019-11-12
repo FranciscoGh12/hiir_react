656294f67b5a8ccb2685f326f5ae38a44e4b777c
Francisco Garcia Hernandez <MaroGh@MacBook-Pro-de-Francisco.local>
2019-11-11 23:59:27 -0600

    Cards2.0

--- src/components/WebCard.js
+++ src/components/WebCard.js
@@ -1,39 +1,52 @@
 import React, { Component } from 'react';
 import backCard1 from '../assets/images/Compu.png';
 import backCard2 from '../assets/images/Foto.png';
+import backCard3 from '../assets/images/fotogestion.png';
 import { Card } from 'react-bootstrap';
-import { Jumbotron, Button } from 'react-bootstrap';
 
 class WebCard extends Component {
     render() {
         return (
             <div className="Cards">
-                                <di className="pesas">
-                    <Jumbotron style={{ backgroundImage: `url(${backCard1})`, backgroundSize:'cover', height:'100%', alignContent:'center' }}>
-                        <h1>Hello, world!</h1>
-                        <p>
-                            This is a simple hero unit, a simple jumbotron-style component for calling
-                            extra attention to featured content or information.
-  </p>
-                        <p>
-                            <Button variant="primary">Learn more</Button>
-                        </p>
-                    </Jumbotron>
-                </di>
                 <div className="compu">
-                    <Card className="bg-dark text-white">
-                        <Card.Img src={backCard2} alt="Card image" />
-                        <Card.ImgOverlay>
-                            <Card.Title>Card title</Card.Title>
-                            <Card.Text>
-                                This is a wider card with supporting text below as a natural lead-in to
-                                additional content. This content is a little bit longer.
+                <Card className="bg-dark text-white">
+                <Card.Img src={backCard1} alt="Card image" />
+                <Card.ImgOverlay>
+                    <Card.Title>Card title</Card.Title>
+                    <Card.Text>
+                        This is a wider card with supporting text below as a natural lead-in to
+                        additional content. This content is a little bit longer.
                     </Card.Text>
-                            <Card.Text>Last updated 3 mins ago</Card.Text>
-                        </Card.ImgOverlay>
-                    </Card>
+                    <Card.Text>Last updated 3 mins ago</Card.Text>
+                </Card.ImgOverlay>
+            </Card>
                 </div>
-
+                <div className="gestion">
+                <Card className="bg-dark text-white">
+                <Card.Img src={backCard3} alt="Card image" />
+                <Card.ImgOverlay>
+                    <Card.Title>Card title</Card.Title>
+                    <Card.Text>
+                        This is a wider card with supporting text below as a natural lead-in to
+                        additional content. This content is a little bit longer.
+                    </Card.Text>
+                    <Card.Text>Last updated 3 mins ago</Card.Text>
+                </Card.ImgOverlay>
+            </Card>
+                </div>
+                <di className="pesas">
+                <Card className="bg-dark text-white">
+                <Card.Img src={backCard2} alt="Card image" />
+                <Card.ImgOverlay>
+                    <Card.Title>Card title</Card.Title>
+                    <Card.Text>
+                        This is a wider card with supporting text below as a natural lead-in to
+                        additional content. This content is a little bit longer.
+                    </Card.Text>
+                    <Card.Text>Last updated 3 mins ago</Card.Text>
+                </Card.ImgOverlay>
+            </Card>
+                </di>
             </div>
 
         );
